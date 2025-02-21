a18892917293f6acc1c3d2774e4fbba6e25d887f
Shaurya094 <Shauryauprari094@gmail.com>
2025-02-21 17:42:31 +1100

    I learnt how Data types work

---
+++ Activity 2.py
@@ -0,0 +1,14 @@
+s = 1
+s = str(s)
+print (s)
+print ("Data type of s:", type(s))
+
+h = 2.1
+h = int(h)
+print (h)
+print ("Data type of h:", type(h))
+
+i = 2
+i = float(i)
+print (i)
+print ("Data type of i:", type (i))
---
+++ Activity 3.py
@@ -0,0 +1,3 @@
+print ("Enter your name:")
+name = input("enter your name: ")
+print ("\nHello", name, "\nWelcome.")
